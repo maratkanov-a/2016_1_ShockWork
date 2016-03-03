192264eace89923ba8308c084da1ddf6e453f974
maratkanov-a <a.s.maratkanov@gmail.com>
2016-03-03 03:05:34 +0300

    added teemail validation

--- public_html/js/models/session.js
+++ public_html/js/models/session.js
@@ -28,6 +28,7 @@ define([
         },
 
         validateRegistration: function(email, username, password1, password2) {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             if ( !(email && username && password1 && password2) ) {
                 $('.form__error').text('required').show();
                 return false
@@ -35,6 +36,10 @@ define([
                 $('.form__error').hide();
                 $('.js-password1-error, .js-password2-error').text('Passwords dont match').show();
                 return false
+            } else if (!re.test(email)) {
+                $('.form__error').hide();
+                $('.js-email-error').text('email must consist @ symbol').show();
+                return false;
             }
             return true;
         }
