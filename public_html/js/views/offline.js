0261bdb7bb9b3127d8e99a3b518fe9f6490d474c
shramru <shramru@gmail.com>
2016-06-02 23:45:30 +0300

    fix backs

--- public_html/js/views/offline.js
+++ public_html/js/views/offline.js
@@ -36,235 +36,235 @@ define([
             this.USER_health =50;
             this.stack_to_delete = [];
             this.user1_stack = [
-            {
-                "id": 1,
-                "img": "bekbulatov_card",
-                "power": 5,
-                "mana": 1
-            },
-            {
-                "id": 2,
-                "img": "burlak_card",
-                "power": 9,
-                "mana": 2
-            },
-            {
-                "id": 3,
-                "img": "didikin_card",
-                "power": 7,
-                "mana": 2
-            },
-            {
-                "id": 4,
-                "img": "dudina_card",
-                "power": 3,
-                "mana": 3
-            },
-            {
-                "id": 5,
-                "img": "frolov_card",
-                "power": 11,
-                "mana": 4
-            },
-            {
-                "id": 6,
-                "img": "isaikin_card",
-                "power": 8,
-                "mana": 5
-            },
-            {
-                "id": 7,
-                "img": "ivanov_card",
-                "power": 4,
-                "mana": 6
-            },
-            {
-                "id": 8,
-                "img": "korepanov_card",
-                "power": 8,
-                "mana": 6
-            },
-            {
-                "id": 9,
-                "img": "mazcevitc_card",
-                "power": 35,
-                "mana": 6
-            },
-            {
-                "id": 10,
-                "img": "meleshenko_card",
-                "power": 4,
-                "mana": 1
-            },
-            {
-                "id": 11,
-                "img": "mezin_card",
-                "power": 6,
-                "mana": 2
-            },
-            {
-                "id": 12,
-                "img": "mogilin_card",
-                "power": 19,
-                "mana": 5
-            },
-            {
-                "id": 13,
-                "img": "petrov_card",
-                "power": 12,
-                "mana": 10
-            },
-            {
-                "id": 14,
-                "img": "sherbinin_card",
-                "power": 61,
-                "mana": 5
-            },
-            {
-                "id": 15,
-                "img": "shubin_card",
-                "power": 45,
-                "mana": 4
-            },
-            {
-                "id": 16,
-                "img": "smal_card",
-                "power": 13,
-                "mana": 1
-            },
-            {
-                "id": 17,
-                "img": "soloviev_card",
-                "power": 9,
-                "mana": 4
-            },
-            {
-                "id": 18,
-                "img": "stupnikov_card",
-                "power": 1,
-                "mana": 5
-            }
-        ];
-            this.AI_stack = [
-                {
-                    "id": 1,
-                    "img": "bekbulatov_card",
-                    "power": 5,
-                    "mana": 1
-                },
-                {
-                    "id": 2,
-                    "img": "burlak_card",
-                    "power": 9,
-                    "mana": 2
-                },
-                {
-                    "id": 3,
-                    "img": "didikin_card",
-                    "power": 7,
-                    "mana": 2
-                },
-                {
-                    "id": 4,
-                    "img": "dudina_card",
-                    "power": 3,
-                    "mana": 3
-                },
-                {
-                    "id": 5,
-                    "img": "frolov_card",
-                    "power": 11,
-                    "mana": 4
-                },
-                {
-                    "id": 6,
-                    "img": "isaikin_card",
-                    "power": 8,
-                    "mana": 5
-                },
-                {
-                    "id": 7,
-                    "img": "ivanov_card",
-                    "power": 4,
-                    "mana": 6
-                },
-                {
-                    "id": 8,
-                    "img": "korepanov_card",
-                    "power": 8,
-                    "mana": 6
-                },
-                {
-                    "id": 9,
-                    "img": "mazcevitc_card",
-                    "power": 35,
-                    "mana": 6
-                },
-                {
-                    "id": 10,
-                    "img": "meleshenko_card",
-                    "power": 4,
-                    "mana": 1
-                },
-                {
-                    "id": 11,
-                    "img": "mezin_card",
-                    "power": 6,
-                    "mana": 2
-                },
-                {
-                    "id": 12,
-                    "img": "mogilin_card",
-                    "power": 19,
-                    "mana": 5
-                },
-                {
-                    "id": 13,
-                    "img": "petrov_card",
-                    "power": 12,
-                    "mana": 10
-                },
-                {
-                    "id": 14,
-                    "img": "sherbinin_card",
-                    "power": 61,
-                    "mana": 5
-                },
-                {
-                    "id": 15,
-                    "img": "shubin_card",
-                    "power": 45,
-                    "mana": 4
-                },
-                {
-                    "id": 16,
-                    "img": "smal_card",
-                    "power": 13,
-                    "mana": 1
-                },
-                {
-                    "id": 17,
-                    "img": "soloviev_card",
-                    "power": 9,
-                    "mana": 4
-                },
-                {
-                    "id": 18,
-                    "img": "stupnikov_card",
-                    "power": 1,
-                    "mana": 5
-                }
-            ];
+	            {
+	                "id": 1,
+	                "img": "bekbulatov_card",
+	                "power": 5,
+	                "mana": 1
+	            },
+	            {
+	                "id": 2,
+	                "img": "burlak_card",
+	                "power": 9,
+	                "mana": 2
+	            },
+	            {
+	                "id": 3,
+	                "img": "didikin_card",
+	                "power": 7,
+	                "mana": 2
+	            },
+	            {
+	                "id": 4,
+	                "img": "dudina_card",
+	                "power": 3,
+	                "mana": 3
+	            },
+	            {
+	                "id": 5,
+	                "img": "frolov_card",
+	                "power": 11,
+	                "mana": 4
+	            },
+	            {
+	                "id": 6,
+	                "img": "isaikin_card",
+	                "power": 8,
+	                "mana": 5
+	            },
+	            {
+	                "id": 7,
+	                "img": "ivanov_card",
+	                "power": 4,
+	                "mana": 6
+	            },
+	            {
+	                "id": 8,
+	                "img": "korepanov_card",
+	                "power": 8,
+	                "mana": 6
+	            },
+	            {
+	                "id": 9,
+	                "img": "mazcevitc_card",
+	                "power": 35,
+	                "mana": 6
+	            },
+	            {
+	                "id": 10,
+	                "img": "meleshenko_card",
+	                "power": 4,
+	                "mana": 1
+	            },
+	            {
+	                "id": 11,
+	                "img": "mezin_card",
+	                "power": 6,
+	                "mana": 2
+	            },
+	            {
+	                "id": 12,
+	                "img": "mogilin_card",
+	                "power": 19,
+	                "mana": 5
+	            },
+	            {
+	                "id": 13,
+	                "img": "petrov_card",
+	                "power": 12,
+	                "mana": 10
+	            },
+	            {
+	                "id": 14,
+	                "img": "sherbinin_card",
+	                "power": 61,
+	                "mana": 5
+	            },
+	            {
+	                "id": 15,
+	                "img": "shubin_card",
+	                "power": 45,
+	                "mana": 4
+	            },
+	            {
+	                "id": 16,
+	                "img": "smal_card",
+	                "power": 13,
+	                "mana": 1
+	            },
+	            {
+	                "id": 17,
+	                "img": "soloviev_card",
+	                "power": 9,
+	                "mana": 4
+	            },
+	            {
+	                "id": 18,
+	                "img": "stupnikov_card",
+	                "power": 1,
+	                "mana": 5
+	            }
+        	];
+	        this.AI_stack = [
+	            {
+	                "id": 1,
+	                "img": "bekbulatov_card",
+	                "power": 5,
+	                "mana": 1
+	            },
+	            {
+	                "id": 2,
+	                "img": "burlak_card",
+	                "power": 9,
+	                "mana": 2
+	            },
+	            {
+	                "id": 3,
+	                "img": "didikin_card",
+	                "power": 7,
+	                "mana": 2
+	            },
+	            {
+	                "id": 4,
+	                "img": "dudina_card",
+	                "power": 3,
+	                "mana": 3
+	            },
+	            {
+	                "id": 5,
+	                "img": "frolov_card",
+	                "power": 11,
+	                "mana": 4
+	            },
+	            {
+	                "id": 6,
+	                "img": "isaikin_card",
+	                "power": 8,
+	                "mana": 5
+	            },
+	            {
+	                "id": 7,
+	                "img": "ivanov_card",
+	                "power": 4,
+	                "mana": 6
+	            },
+	            {
+	                "id": 8,
+	                "img": "korepanov_card",
+	                "power": 8,
+	                "mana": 6
+	            },
+	            {
+	                "id": 9,
+	                "img": "mazcevitc_card",
+	                "power": 35,
+	                "mana": 6
+	            },
+	            {
+	                "id": 10,
+	                "img": "meleshenko_card",
+	                "power": 4,
+	                "mana": 1
+	            },
+	            {
+	                "id": 11,
+	                "img": "mezin_card",
+	                "power": 6,
+	                "mana": 2
+	            },
+	            {
+	                "id": 12,
+	                "img": "mogilin_card",
+	                "power": 19,
+	                "mana": 5
+	            },
+	            {
+	                "id": 13,
+	                "img": "petrov_card",
+	                "power": 12,
+	                "mana": 10
+	            },
+	            {
+	                "id": 14,
+	                "img": "sherbinin_card",
+	                "power": 61,
+	                "mana": 5
+	            },
+	            {
+	                "id": 15,
+	                "img": "shubin_card",
+	                "power": 45,
+	                "mana": 4
+	            },
+	            {
+	                "id": 16,
+	                "img": "smal_card",
+	                "power": 13,
+	                "mana": 1
+	            },
+	            {
+	                "id": 17,
+	                "img": "soloviev_card",
+	                "power": 9,
+	                "mana": 4
+	            },
+	            {
+	                "id": 18,
+	                "img": "stupnikov_card",
+	                "power": 1,
+	                "mana": 5
+	            }
+	        ];
 
-            this.userStackTable = $(".score span.my");
+	        this.userStackTable = $(".score span.my");
 
-            this.$el.html(this.template());
+	        this.$el.html(this.template());
 
-            this.shuffle(this.user1_stack);
+	        this.shuffle(this.user1_stack);
 
-            this.shuffle(this.AI_stack); // вот в этот массив апиха отдает то, что выкинул юзер или ИИ
-            this.init_table();
-            this.draw(this.user1_stack);
+	        this.shuffle(this.AI_stack); // вот в этот массив апиха отдает то, что выкинул юзер или ИИ
+	        this.init_table();
+	        this.draw(this.user1_stack);
         },
         show: function() {
             this.render();
